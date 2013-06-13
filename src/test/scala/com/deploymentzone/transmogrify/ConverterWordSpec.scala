c89a3c3c8e142ea35d0dfe88810cb37fad029bf2
Charles Feduke <charles.feduke@gmail.com>
2013-06-12 22:50:46 -0400

    only alpha-end test cases enabled

--- src/test/scala/com/deploymentzone/transmogrify/ConverterWordSpec.scala
+++ src/test/scala/com/deploymentzone/transmogrify/ConverterWordSpec.scala
@@ -25,26 +25,26 @@ class ConverterWordSpec extends WordSpec with ShouldMatchers {
     }
   }
 
-  "convert when converting decimal to binary encoding" should {
-
-    "return '111' when passed 7." in {
-      convert(7) should equal("111")
-    }
-
-  }
-
-  "convert when converting decimal to octal encoding" should {
-
-    "return '55' when passed 45." in {
-      convert(45) should equal("55")
-    }
-
-  }
-
-  "convert when converting decimal to hex encoding" should {
-
-    "return '1f' when passed 31." in {
-      convert(31) should equal("1F")
-    }
-  }
+//  "convert when converting decimal to binary encoding" should {
+//
+//    "return '111' when passed 7." in {
+//      convert(7) should equal("111")
+//    }
+//
+//  }
+//
+//  "convert when converting decimal to octal encoding" should {
+//
+//    "return '55' when passed 45." in {
+//      convert(45) should equal("55")
+//    }
+//
+//  }
+//
+//  "convert when converting decimal to hex encoding" should {
+//
+//    "return '1f' when passed 31." in {
+//      convert(31) should equal("1F")
+//    }
+//  }
 }
