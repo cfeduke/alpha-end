cd58477206b6e55ce7edb430d5a9c86b8a2d04b2
Charles Feduke <charles.feduke@gmail.com>
2013-06-12 22:52:19 -0400

    single degenerate passing test

--- src/main/scala/com/deploymentzone/transmogrify/Converter.scala
+++ src/main/scala/com/deploymentzone/transmogrify/Converter.scala
@@ -1,13 +1,7 @@
 package com.deploymentzone.transmogrify
 
 object Converter {
-  def convert(number: Int) : String = number match {
-    case 13 => "10"
-    case 20 => "17"
-    case 5006 => "2381"
-    case 9999999 => "20z1879"
-    case 7 => "111"
-    case 45 => "55"
-    case 31 => "1F"
+  def convert(number: Int) : String = {
+    "10"
   }
 }
