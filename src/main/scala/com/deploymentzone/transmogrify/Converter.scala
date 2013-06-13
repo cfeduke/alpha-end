e062d427122ffc924c66430cf32edee65402f733
Charles Feduke <charles.feduke@gmail.com>
2013-06-12 23:34:46 -0400

    initial implementation passing the first test

--- src/main/scala/com/deploymentzone/transmogrify/Converter.scala
+++ src/main/scala/com/deploymentzone/transmogrify/Converter.scala
@@ -1,7 +1,29 @@
 package com.deploymentzone.transmogrify
 
 object Converter {
+
+  val Base13 = 13
+
   def convert(number: Int) : String = {
-    "10"
+    def repeatedDivision(accumulator: StringBuilder, dividend: Int): String = {
+      // converge
+      if (dividend < getRadix) {
+        accumulator.append(dividend)
+        return accumulator.mkString("").reverse
+      }
+      // accumulate
+      val result = new Division(dividend)
+      accumulator.append(result.remainder)
+      repeatedDivision(accumulator, result.quotient)
+    }
+
+    repeatedDivision(new StringBuilder(), number)
+  }
+
+  def getRadix = Base13
+
+  private class Division(val dividend: Int) {
+    val quotient: Int = dividend / getRadix
+    val remainder: Int = dividend % getRadix
   }
 }
